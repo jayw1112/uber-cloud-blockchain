8979e7dcfb40a3467ac7b1da54b9549cbeaad86d
jayw1112 <housexpresso@gmail.com>
2022-03-15 16:41:40 -0700

    edited navbar and made box resizable & opaque

--- components/Navbar.js
+++ components/Navbar.js
@@ -8,11 +8,11 @@ const style = {
   wrapper: `h-16 w-full bg-black text-white flex md:justify-around items-center px-60 fixed z-20`,
   leftMenu: `flex gap-3`,
   logo: `text-3xl text-white flex cursor-pointer mr-16`,
-  menuItem: `text-lg text-white font-medium flex items-center mx-4 cursor-pointer`,
+  menuItem: `text-lg text-white font-medium items-center mx-4 cursor-pointer hidden md:flex`,
   rightMenu: `flex gap-3 items-center`,
   userImageContainer: `mr-2`,
   userImage: `h-10 w-10 mr-4 rounded-full p-px object-cover cursor-pointer`,
-  loginButton: `flex items-center cursor-pointer rounded-full hover:bg-[#333333] px-4 py-1`,
+  loginButton: `items-center cursor-pointer rounded-full hover:bg-[#333333] px-4 py-1 hidden md:flex`,
   loginText: `ml-2`,
 }
 
